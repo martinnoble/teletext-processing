5f83b8ed41f8b4a24656323f3bbeaf788f1f2c3c
Martin Noble <martin@martinnoble.com>
2026-07-20 10:38:00 +0100

    correct ITV time format

--- start_itv.sh
+++ start_itv.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 sudo /home/museum/raspi-teletext/tvctl on
-python3 t42restream.py ./t42/ITV-19890114-sq.t42 --time-format "%H:%M/%S" --magazine-parallel --control interrupted-sequence=off --loop | /home/museum/raspi-teletext/teletext -m 0xff00 -l 66 -
+python3 t42restream.py ./t42/ITV-19890114-sq.t42 --time-format " %H%M:%S" --magazine-parallel --control interrupted-sequence=off --loop | /home/museum/raspi-teletext/teletext -m 0xff00 -l 66 -
